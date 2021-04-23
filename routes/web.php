44e38a9a9c499218fb114d51ea937fab963a4d76
shuuto.kakiuchi <kakiuchi.s@78it.com>
2021-04-23 09:40:34 +0000

    follow,unfollow

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,6 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
 
 Route::get('/', 'MicropostsController@index');
 
@@ -27,3 +18,17 @@ Route::group(['middleware' => ['auth']], function () {
     Route::resource('microposts', 'MicropostsController', ['only' => ['store', 'destroy']]);
 
 });
+
+Route::group(['middleware' => ['auth']], function () {
+    Route::group(['prefix' => 'users/{id}'], function () {
+        Route::post('follow', 'UserFollowController@store')->name('user.follow');
+        Route::delete('unfollow', 'UserFollowController@destroy')->name('user.unfollow');
+        Route::get('followings', 'UsersController@followings')->name('users.followings');
+        Route::get('followers', 'UsersController@followers')->name('users.followers');
+    });
+
+    Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+
+    Route::resource('microposts', 'MicropostsController', ['only' => ['store', 'destroy']]);
+});
+
